8ee5da57e2dca8ca4f038a64bb346a6bb751d81b
TheScarecrxw <freestylerdude2006@gmail.com>
2022-12-07 22:42:31 -0500

    Update main.js

--- G11 Computer Sciencce P4/Grade-11 Unit 2/MaxLiaoA2/i am dumb and forgot to commit my code/scripts/main.js
+++ G11 Computer Sciencce P4/Grade-11 Unit 2/MaxLiaoA2/i am dumb and forgot to commit my code/scripts/main.js
@@ -51,26 +51,43 @@ let computerIndex = 1;
 let correctWord;
 let randomWord;
 
-let firstChar;
+// index of the textbox to compare textbox letter with stringfront letter  
 let indexOfTextBox = 0;
+
+// word that is chosen and displayed to the user
 let wordDisplayed;
+
+// action values to differentiate which action was done 
 let normalAttackValue;
 let specialAttackValue;
 let healValue; 
 
+// last character of the string for when the user deletes a letter
 let lastCharacter;
 
+// stores the correct amount of letters the user has typed  
 let savedCorrectLetters = 0;
+
+// user input textbox value length
 let textBoxValueLength = 0;
+
+// find the last occurance of a string for when .replace is used
 let lastOccurance;
+
+// correct letters the user has typed into the text box
 let correctLetters;
 
+// stores the users input value 
 let textBoxValue = I_INPUT_TEXT.value; 
 
+// timer loop for my update function which updates every 20ms
 setInterval(update, 20);
 
+// start function for when the game initially starts
 start();
 
+// function to display everything that is needed when user runs the code
+// displaying characters, creating characters, showing characters ect. 
 function start() {
     createCharacters();
     displayCharacters(currentIndex);
@@ -79,6 +96,8 @@ function start() {
     selectedElementsComputer(computerIndex);
 }
 
+// function to set the values for each characters health, normal attack, special attack, and heal power
+// Zer and Loz, only 2 characters, also sets image and character name
 function createCharacters() {
     characters[HUMAN] = "Zer";
     characterImgFileName[HUMAN] = "Character2.gif";
@@ -95,18 +114,21 @@ function createCharacters() {
     healPower[COMPUTER] = 12;
 }
 
+// shows the current image that needs to be dispalyed when traveling through the traversal
 function showCurrentImage() {
     IMG_TAG.src = characterImgFileName[currentIndex];
     IMG_TAG.width = 350;
     IMG_TAG.height = 350;
 }
 
+// displays the characters stats, image, and name to the user so that the user can choose which character to select 
 function displayCharacters(index) {
     P_CHARACTER_LIST.innerText = characters[index];
     showCurrentImage();
     P_CHARACTER_STATS.innerText = "Health: " + health[index] + "\n\n" + "Normal Attack Power: " + normalAttackPower[index] + "\n\n" + "Special Attack Power: " + specialAttackPower[index] + "\n\n" + "Heal Power: " +  healPower[index];
 }
 
+// used to go to the next character in the traversal 
 function nextCharacter() {
     currentIndex += 1;
     if (currentIndex == characters.length) {
@@ -115,6 +137,7 @@ function nextCharacter() {
     displayCharacters(currentIndex);
 }
 
+// used to go to the previous character in the traversal
 function previousCharacter() {
     currentIndex -= 1;
     if (currentIndex < 0) {
@@ -123,11 +146,14 @@ function previousCharacter() {
     displayCharacters(currentIndex);
 }
 
+// used to display the character the player/human chooses 
 function selectedElementsHuman(chosen) {
     SELECTED_IMG_HUMAN.src = characterImgFileName[chosen];
     SELECTED_CHARACTER_LIST.innerText = "Health: " + health[chosen] + "\n\n" + "Normal Attack Power: " + normalAttackPower[chosen] + "\n\n" + "Special Attack Power: " + specialAttackPower[chosen] + "\n\n" + "Heal Power: " +  healPower[chosen];
 }
 
+// used to dispaly the character that the computer has to take 
+// if the player/human chooses one option the computer takes the other option
 function selectedElementsComputer(chosen) {
     if (currentIndex == 0) {
         chosen = 1
@@ -142,6 +168,8 @@ function selectedElementsComputer(chosen) {
     }
 }
 
+// used to display character's stats in the game phase
+// player is the index of the character 
 function displayCharacter(player) {
     if (player == computerIndex) {
         COMPUTER_CHARACTER_LIST.innerText = "Health : " + health[player] + "\n\n" + "Normal Attack Power: " + normalAttackPower[player] + "\n\n" + "Special Attack Power: " + specialAttackPower[player] + "\n\n" + "Heal Power: " +  healPower[player];
@@ -151,33 +179,43 @@ function displayCharacter(player) {
     }
 }
 
+// function for the computer's normal attacks 
 function autoAttackNormal() {
     normalAttack(currentIndex);
     displayCharacter(currentIndex);
 }
 
+// function for the computer's special attacks 
 function autoAttackSpecial() {
     specialAttack(currentIndex);
     displayCharacter(currentIndex);
 }
  
+// function for the computer's heal power 
 function autoHeal() {
     heal(computerIndex);
     displayCharacter(computerIndex);
 }
 
+// function for when the user selects a character to play
+
 function selectCharacter() {
+    // hids elements in page 1 and reveals elements in page 2 
     document.getElementById("Page1").hidden = true;
     document.getElementById("Page2").hidden = false;
+
+    // used to determine what index the computer will play as 
     if (currentIndex == 0) {
         computerIndex = 1
     }
     else {
         computerIndex = 0
     }
+    // displays the characthers for the user and the computer 
     selectedElementsHuman(currentIndex);
     selectedElementsComputer(computerIndex);
     
+    // sets timer loops for the computers automated actions 
     setInterval(autoAttackNormal, 5000);
 
     setInterval(autoAttackSpecial, 15000);
@@ -185,98 +223,114 @@ function selectCharacter() {
     setInterval(autoHeal, 10000);
 }
 
+// function to normal attack 
 function normalAttack(playerIndex) {
+    // if the parameter is equal to the current/human index it will do the computers damage 
     if (playerIndex == currentIndex) {
         health[playerIndex] -= showRandom(normalAttackPower[computerIndex], normalAttackPower[computerIndex] * (showRandom(0, WORDLIST.length) + 2) / 2) * 2;
     }
+
+    // if the parameters is equal to the computers index it will do the players damage 
     else {
         health[playerIndex] -= showRandom(normalAttackPower[currentIndex], normalAttackPower[currentIndex] * (WORDLIST.indexOf(wordDisplayed) + 2) / 2);
     }
+    // updates the players stats 
     displayCharacter(playerIndex);
 }
 
 function specialAttack(playerIndex) {
+    // if the parameter is equal to the current/human index it will do the computers damage 
     if (playerIndex == currentIndex) {
         health[playerIndex] -= showRandom(specialAttackPower[computerIndex], specialAttackPower[computerIndex] * (showRandom(0, WORDLIST.length) + 2) / 2);
     }
+
+    // if the parameters is equal to the computers index it will do the players damage 
     else {
         health[playerIndex] -= showRandom(specialAttackPower[currentIndex], specialAttackPower[currentIndex] * (WORDLIST.indexOf(wordDisplayed) + 2) / 2);
     }
+    // updates the players stats 
     displayCharacter(playerIndex);
 }
 
 function heal(playerIndex) {
+    // if the parameter is equal to the current/human index it will heal the humans heal power 
     if (playerIndex == currentIndex) {
-        health[playerIndex] -= showRandom(healPower[computerIndex], healPower[computerIndex] * (showRandom(0, WORDLIST.length) + 2) / 2);
+        health[playerIndex] += showRandom(healPower[currentIndex], healPower[currentIndex] * (WORDLIST.indexOf(wordDisplayed) + 2) / 2);
     }
+
+    // if the parameter is equal to the computer index it will heal the computers heal power 
     else {
-        health[playerIndex] -= showRandom(healPower[currentIndex], healPower[currentIndex] * (WORDLIST.indexOf(wordDisplayed) + 2) / 2);
+        health[playerIndex] += showRandom(healPower[computerIndex], healPower[computerIndex] * (showRandom(0, WORDLIST.length) + 2) / 2);
     }
     displayCharacter(playerIndex);
 }
 
 function update() {
     textBoxValue = I_INPUT_TEXT.value;
+    if (textBoxValueLength >= 0 && (normalAttackValue == true || specialAttackValue == true || healValue == true)) {
+        textBoxValueLength = textBoxValue.length;
 
-    if (textBoxValue[indexOfTextBox] == getStringFront(randomWord)) {
-        savedCorrectLetters = textBoxValue.length;
-        lastCharacter = getLastCharacter(textBoxValue);
-        randomWord = getStringBack(randomWord); 
-        correctWord = randomWord;
-        CORRECT_DISPLAY.style.color = "green";
-        CORRECT_DISPLAY.innerText = correctWord;
-        indexOfTextBox ++;
-    }
-    else if (savedCorrectLetters - 1 == textBoxValueLength){
-        correctWord = lastCharacter + correctWord
-        lastCharacter = getLastCharacter(textBoxValue)
-        CORRECT_DISPLAY.style.color = "green";
-        CORRECT_DISPLAY.innerText = correctWord;
-        INCORRECT_DISPLAY.innerText = "";
-        savedCorrectLetters = textBoxValue.length;
-        getStringFront(correctWord)
-        randomWord = wordDisplayed.replace(correctWord, "")
-        randomWord = wordDisplayed.replace(randomWord,"")
-        indexOfTextBox -- 
-    }
-    else if(textBoxValue[0] == lastCharacter){
-        CORRECT_DISPLAY.innerText = correctWord;
-    }
-    else if (textBoxValueLength == 0){
-        randomWord = wordDisplayed
-        indexOfTextBox = 0
-    }
-    else if (textBoxValue == wordDisplayed) {
-        I_INPUT_TEXT.value = "";
-        P_NUMBER.innerText = "";
-        if (normalAttackValue == true) {
-            normalAttack(computerIndex);
-            randomWord = null;
-            normalAttackValue = false;
+        if (textBoxValue[indexOfTextBox] == getStringFront(randomWord)) {
+            savedCorrectLetters = textBoxValue.length;
+            lastCharacter = getLastCharacter(textBoxValue);
+            randomWord = getStringBack(randomWord); 
+            correctWord = randomWord;
+            CORRECT_DISPLAY.style.color = "green";
+            CORRECT_DISPLAY.innerText = correctWord;
+            indexOfTextBox ++;
         }
-        else if (specialAttackValue == true){
-            specialAttack(computerIndex);
-            randomWord = null;
-            specialAttackValue = false;
+        else if (savedCorrectLetters - 1 == textBoxValueLength){
+            correctWord = lastCharacter + correctWord
+            lastCharacter = getLastCharacter(textBoxValue)
+            CORRECT_DISPLAY.style.color = "green";
+            CORRECT_DISPLAY.innerText = correctWord;
+            INCORRECT_DISPLAY.innerText = "";
+            savedCorrectLetters = textBoxValue.length;
+            getStringFront(correctWord)
+            randomWord = wordDisplayed.replace(correctWord, "")
+            randomWord = wordDisplayed.replace(randomWord,"")
+            indexOfTextBox -- 
         }
-        else if (healValue == true) {
-            heal(currentIndex);
-            randomWord = null;
-            healValue = false;
+        else if(textBoxValue[0] == lastCharacter){
+            CORRECT_DISPLAY.innerText = correctWord;
         }
+        else if (textBoxValueLength == 0){
+            randomWord = wordDisplayed
+            indexOfTextBox = 0
+        }
+        else if (textBoxValue == wordDisplayed) {
+            I_INPUT_TEXT.value = "";
+            P_NUMBER.innerText = "";
+            if (normalAttackValue == true) {
+                normalAttack(computerIndex);
+                randomWord = null;
+                normalAttackValue = false;
+            }
+            else if (specialAttackValue == true){
+                specialAttack(computerIndex);
+                randomWord = null;
+                specialAttackValue = false;
+            }
+            else if (healValue == true) {
+                heal(currentIndex);
+                randomWord = null;
+                healValue = false;
+            }
+        }
+        else {
+            lastOccurance = wordDisplayed.lastIndexOf(correctWord)
+            correctLetters = wordDisplayed.substring(0, lastOccurance);
+            INCORRECT_DISPLAY.style.color = "red";
+            INCORRECT_DISPLAY.innerText = textBoxValue.replace(correctLetters, "");
+        }
+        
+        if (textBoxValue.length == 0) {
+            CORRECT_DISPLAY.innerText = "";
+            INCORRECT_DISPLAY.innerText = "";
+        }
+        gameOver();
     }
-    else {
-        lastOccurance = wordDisplayed.lastIndexOf(correctWord)
-        correctLetters = wordDisplayed.substring(0, lastOccurance);
-        INCORRECT_DISPLAY.style.color = "red";
-        INCORRECT_DISPLAY.innerText = textBoxValue.replace(correctLetters, "");
-    }
-    }
-    if (textBoxValue.length == 0) {
-        CORRECT_DISPLAY.innerText = "";
-        INCORRECT_DISPLAY.innerText = "";
-    }
-    gameOver();
+}
 
 function showWord(action) {
     randomWord = WORDLIST[showRandom(0, WORDLIST.length - 1)];
@@ -320,5 +374,5 @@ function gameOver() {
 }
 
 function restart(){
-    window.location.reload();
+    //location.href = "index.html"
 }
